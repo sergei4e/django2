9055df296ec060229258db314ca2bdcd82da4504
Sergey Chernenko <4e.sergei@gmail.com>
2020-01-17 11:03:56 +0200

    bug fix (handler)

--- news/views.py
+++ news/views.py
@@ -1,37 +1,37 @@
 from django.shortcuts import render
 
 
-def blog_hendler(request):
+def blog_handler(request):
     context = {}
     return render(request, 'news/blog.html', context)
 
 
-def page_hendler(request):
+def page_handler(request):
     context = {}
     return render(request, 'news/page.html', context)
 
 
-def contact_hendler(request):
+def contact_handler(request):
     context = {}
     return render(request, 'news/contact.html', context)
 
 
-def about_hendler(request):
+def about_handler(request):
     context = {}
     return render(request, 'news/about.html', context)
 
 
-def index_hendler(request):
+def index_handler(request):
     context = {}
     return render(request, 'news/index.html', context)
 
 
-def search_hendler(request):
+def search_handler(request):
     context = {}
     return render(request, 'news/search.html', context)
 
 
-def robots_hendler(request):
+def robots_handler(request):
     context = {}
     return render(request, 'news/robots.txt', context,
                   content_type='text/plain')
